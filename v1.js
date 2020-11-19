2e7c0cd4756f1ab463be42b5227401202b3354e7
Temuulen Ganbaatar <ganbaatar.t03@htlwienwest.at>
2020-11-19 09:00:09 +0100

    cr

--- v1.js
+++ v1.js
@@ -1,9 +1,21 @@
-//console.log("hallo")
+const numberCards = 16
 
-let c = document.querySelectorAll('.card')
-console.log(c)
+let p = document.querySelector('.deck')
 
+for (let index = 0; index < numberCards; index++)
+{
+    let c = document.createElement('div')
+    c.innerHTML = index % 3 + 1
+    c.addEventListener('click', flip)
+    c.className="card"
+    
+    p.appendChild(c)
 
+}
 
+function flip(event) {
+    //alert("test")
+    this.classList.toggle('flipped')
+}
 
 
