df18567213a66f27f68730b7f3e1bded321ac43b
Temuulen Ganbaatar <ganbaatar.t03@htlwienwest.at>
2020-11-26 08:49:43 +0100

    a

--- v1.js
+++ v1.js
@@ -4,6 +4,7 @@
 3) Karten mischen (shuffle)
 4) Karten zum Deck hinzufügen
 5) Klick Eventhandler
+----
 6) Kartenlogik
     6.1) prüfen wie viele Karten aufgedeckt sind? (0,1,2)
     6.2) Wenn weniger als 2 -> aufdecken
@@ -14,13 +15,12 @@
 const numberCards = 16
 //1)
 let openedCards = []
-
-let p = document.querySelector('.deck')
 //
 //2)
 //TODO: Array erzeugen mit durchmischten Karten (16 Karten, 8 Typen(1-8))
-const randomarray = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
 
+/*  gemischte Karten aber nicht als function und nicht abhängig von numbercard
+const randomarray = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
 for(let i = randomarray.length-1; i>0; i--){
     const j = Math.floor(Math.random() * i)
     const temp = randomarray[i]
@@ -28,8 +28,13 @@ for(let i = randomarray.length-1; i>0; i--){
     randomarray[j] = temp
   }
 console.log(randomarray)
-//
-//
+*/
+let cardTypes=[]
+for (let index = 0; index < numberCards/2; index++) {
+    cardTypes.push(index+1)
+    cardTypes.push(index+1)
+}
+
 
 for (let index = 0; index < numberCards; index++) {
         let c = document.createElement('div')
@@ -40,13 +45,31 @@ for (let index = 0; index < numberCards; index++) {
         p.appendChild(c)
 }
 
+
 function flipp(event){
     if(!this.classList.contains('found'))
         openCard(this)
     //this.classList.toggle('flipped')
 }
+
+
 function openCard(c){
     alert(c.innerHTML)
 }
 
 
+function shuffle(arra1) {
+    var ctr = arra1.length, temp, index;
+// While there are elements in the array
+    while (ctr > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+        ctr--;
+// And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
+}
